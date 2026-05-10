68e6c3d8d0324da3f5666f62858bf6a43a64e31b
Jonathan Atmaja <jonathan.atmaja@byorange.co.id>
2026-05-10 22:23:51 +0700

    Show favorite icon

--- features/home/components/pokemon-card.tsx
+++ features/home/components/pokemon-card.tsx
@@ -2,18 +2,32 @@ import { Card, Typography } from "@mui/material";
 import Image from "next/image";
 import { pokemonCardSty } from "../styles";
 
+import { DEFAULT_IMG_WIDTH_HEIGHT } from "@/lib/constants";
+import { Star, StarBorder } from "@mui/icons-material";
+
 interface PokemonCardProps {
   name: string;
   sprite?: string;
   url: string;
+  isFavorite?: boolean;
 }
 
 export const PokemonCard = (props: PokemonCardProps) => {
-  const { name, sprite } = props;
+  const { name, sprite, isFavorite } = props;
 
   return (
     <Card variant="outlined" elevation={0} sx={pokemonCardSty}>
-      <Image src={String(sprite)} alt="sprite" width={125} height={125} />
+      {isFavorite ? (
+        <Star style={{ color: "gold" }} sx={{ alignSelf: "flex-end" }} />
+      ) : (
+        <StarBorder style={{ color: "gold" }} sx={{ alignSelf: "flex-end" }} />
+      )}
+      <Image
+        src={String(sprite)}
+        alt="sprite"
+        width={DEFAULT_IMG_WIDTH_HEIGHT}
+        height={DEFAULT_IMG_WIDTH_HEIGHT}
+      />
       <Typography
         sx={{ alignSelf: "center", fontWeight: "bolder" }}
         variant="button"
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_LIMIT = 50;
 export const DEFAULT_OFFSET = 0;
+export const DEFAULT_IMG_WIDTH_HEIGHT = 125;
